a77580588d3f2043f5074f01779c951bac857ca8
hussainibulama <43305254+hussainibulama@users.noreply.github.com>
2020-07-03 08:24:56 +0100

    Added my project

---
+++ build/precache-manifest.b2136088e898cb08b1d680b2dab06415.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d08a826c6a5025bdc88553d7f6b2e65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64269bf7cf5c049f978b",
+    "url": "/static/css/main.2eadbc01.chunk.css"
+  },
+  {
+    "revision": "baa42e09a9b67328327f",
+    "url": "/static/js/2.b11f8d47.chunk.js"
+  },
+  {
+    "revision": "7d6b40073579b814286a6a4cc2e947db",
+    "url": "/static/js/2.b11f8d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64269bf7cf5c049f978b",
+    "url": "/static/js/main.665a7f4d.chunk.js"
+  },
+  {
+    "revision": "5e1354b178bc063acab5",
+    "url": "/static/js/runtime-main.08cfc7ed.js"
+  },
+  {
+    "revision": "ab0e85ee2de98383ba1da90387bc6614",
+    "url": "/static/media/backg.ab0e85ee.svg"
+  },
+  {
+    "revision": "3242db76f2d2b3e2c740353fcafb6349",
+    "url": "/static/media/background.3242db76.jpg"
+  },
+  {
+    "revision": "7a2a216b0fae3ad34394ec7e938ae50d",
+    "url": "/static/media/box11.7a2a216b.png"
+  },
+  {
+    "revision": "ba19b816bc814b803800ad434d947bcf",
+    "url": "/static/media/logb.ba19b816.svg"
+  },
+  {
+    "revision": "a7070a62a67624ce787f4ee802b44391",
+    "url": "/static/media/pos-bg.a7070a62.png"
+  }
+]);
---
+++ router.js
@@ -0,0 +1,264 @@
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+
+class Router {
+  constructor(app, db) {
+    this.login(app, db);
+    this.verifyAccount(app, db);
+    this.UpdatePersonal(app, db);
+    this.UpdatePassword(app, db);
+    this.logout(app, db);
+    this.isLoggedIn(app, db);
+    this.fetchhistory(app, db);
+    this.fetchhistories(app, db);
+    this.DeleteCard(app, db);
+  }
+  login(app, db) {
+    app.post("/login", (req, res) => {
+      let username = req.body.username;
+      let password = req.body.password;
+      console.log(username, password);
+
+      username = username.toLowerCase();
+      let cols = [username];
+      db.query(
+        "SELECT * FROM biker WHERE username = ? LIMIT 1",
+        cols,
+        (err, data, fields) => {
+          if (err) {
+            res.json({
+              success: false,
+              msg: "an error occured, please try again later",
+            });
+            return;
+          }
+          if (data && data.length === 1) {
+            bcrypt.compare(
+              password,
+              data[0].password,
+              (bcryptErr, verified) => {
+                if (verified) {
+                  req.session.userID = data[0].id;
+                  res.json({
+                    success: true,
+                    sessionID: data[0].id,
+                    username: cols,
+                    name: data[0].name,
+                    state: data[0].state,
+                    address: data[0].address,
+                  });
+                  return;
+                } else {
+                  res.json({
+                    success: false,
+                    msg: "invalid  password",
+                  });
+                  return;
+                }
+              }
+            );
+          } else {
+            res.json({
+              success: false,
+              msg: "invalid username or password",
+            });
+            return;
+          }
+        }
+      );
+    });
+  }
+  verifyAccount(app, db) {
+    app.post("/createbiker", (req, res) => {
+      db.query(
+        "INSERT INTO biker SET ?",
+        {
+          username: req.body.username,
+          password: bcrypt.hashSync(req.body.password, 9),
+        },
+        (err, data, fields) => {
+          if (err) {
+            res.json({
+              success: false,
+              msg: "an error occured, please try again later",
+            });
+            return;
+          }
+          if (data) {
+            res.json({
+              success: true,
+              msg: "account Created",
+            });
+            return;
+          } else {
+            res.json({
+              success: false,
+              msg: "you have an error with your form",
+            });
+            return;
+          }
+        }
+      );
+    });
+  }
+  UpdatePersonal(app, db) {
+    app.post("/updatetracking", (req, res) => {
+      db.query(
+        "UPDATE pickup SET status='" +
+          req.body.status +
+          "' WHERE trackingid = ?",
+        req.body.trackingid,
+        (err, data, fields) => {
+          if (err) {
+            res.json({
+              success: false,
+              msg: "an error occured, please try again later",
+            });
+            return;
+          }
+          if (data) {
+            res.json({
+              success: true,
+              msg: "Updated Successfully",
+            });
+            return;
+          } else {
+            res.json({
+              success: false,
+              msg: "server error, try later",
+            });
+            return;
+          }
+        }
+      );
+    });
+  }
+  UpdatePassword(app, db) {
+    app.post("/assign", (req, res) => {
+      let trackingid = req.body.trackingid;
+      db.query(
+        "UPDATE pickup SET bikerinfo='" +
+          req.body.biker +
+          "' WHERE trackingid= ?",
+        trackingid,
+        (err, data, fields) => {
+          if (err) {
+            res.json({
+              success: false,
+              msg: "an error occured, please try again later",
+            });
+            return;
+          }
+          if (data) {
+            res.json({
+              success: true,
+              msg: "Assign Successful",
+            });
+            return;
+          } else {
+            res.json({
+              success: false,
+              msg: "server error, try later",
+            });
+            return;
+          }
+        }
+      );
+    });
+  }
+
+  logout(app, db) {
+    app.post("/logout", (req, res) => {
+      if (req.session.userID) {
+        req.session.destroy();
+        res.json({
+          success: true,
+        });
+
+        return;
+      } else {
+        res.json({
+          success: false,
+        });
+        return;
+      }
+    });
+  }
+  isLoggedIn(app, db) {
+    app.post("/isLoggedIn", (req, res) => {
+      if (req.session.userID) {
+        let cols = [req.session.userID];
+        db.query(
+          "SELECT * FROM biker WHERE id = ? LIMIT 1",
+          cols,
+          (err, data, fields) => {
+            if (data && data.length === 1) {
+              res.json({
+                success: true,
+                username: data[0].username,
+              });
+              return true;
+            } else {
+              res.json({
+                success: false,
+              });
+            }
+          }
+        );
+      } else {
+        res.json({
+          success: false,
+        });
+      }
+    });
+  }
+  fetchhistory(app, db) {
+    app.post("/fetchpickup", (req, res) => {
+      db.query(
+        "SELECT * FROM pickup WHERE bikerinfo = ?",
+        req.body.username,
+        (err, data, fields) => {
+          if (data) {
+            res.end(JSON.stringify(data));
+          } else {
+            res.json({
+              success: false,
+              msg: "No data",
+            });
+          }
+        }
+      );
+    });
+  }
+  fetchhistories(app, db) {
+    app.post("/fetchpickuphistory", (req, res) => {
+      let id = req.body.sessionID;
+      db.query("SELECT * FROM pickup ", (err, data, fields) => {
+        if (data) {
+          res.end(JSON.stringify(data));
+        } else {
+          res.json({
+            success: false,
+            msg: "No data",
+          });
+        }
+      });
+    });
+  }
+  DeleteCard(app, db) {
+    app.post("/fetchbikers", (req, res) => {
+      let id = req.body.sessionID;
+      db.query("SELECT username FROM biker ", (err, data, fields) => {
+        if (data) {
+          res.end(JSON.stringify(data));
+        } else {
+          res.json({
+            success: false,
+            msg: "No data",
+          });
+        }
+      });
+    });
+  }
+}
+module.exports = Router;
